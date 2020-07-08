0d8da6cc8c52b8fa0ffd7c21c211c88cef92daac
dreadheaddreamz <github email address>
2020-07-07 21:42:36 -0500

    added signup feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
     has_many :reviews
     has_many :shoes, through: :reviews
 
     has_many :kicks
+    validates :username, uniqueness: true,  presence: true
+    has_secure_password
+    
 end
