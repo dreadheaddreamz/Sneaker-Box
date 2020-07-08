f708dc44be7289f57f4ea4f3c0bfb6c9b1338a49
dreadheaddreamz <github email address>
2020-07-08 18:56:04 -0500

    added login and authenticate feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,16 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry wrong username/password"
+            redirect_to login_path
+        end
     end
+
 
     def destroy
         session.delete(:user_id)
