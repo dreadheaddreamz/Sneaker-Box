d17a0920762579f1411b6455e1c9c2da75ece5bb
dreadheaddreamz <github email address>
2020-07-08 20:03:08 -0500

    added featue to add shoes and brands

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,22 @@
 class ShoesController < ApplicationController
+    def new
+        @shoe = Shoe.new
+        @shoe.build_brand
+    end
+
+    def create
+        @shoe = Shoe.new(shoe_params)
+        @shoe.user_id = session[:user_id]
+        if @shoe.save
+          redirect_to shoe_path(@shoe)
+        else
+          render :new
+        end
+      end
+
+    private
+
+    def shoe_params
+        params.require(:shoe).permit(:name, :description, :shoe_id, brand_attributes: [:name])
+    end
 end
