0e07550925716ed65c576389ea26ac6ee3fb1076
dreadheaddreamz <github email address>
2020-07-09 19:35:06 -0500

    added reviews feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+    def new
+    if
+        @shoe = Shoe.find_by_id(params[:shoe_id])
+        @review = @shoe.reviews.build
+    else
+        @review = Review.new
+        end
+    end
+
+    def index
+    end
+
+
+
+    private
+    def set_shoe
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   delete '/logout' => 'sessions#destroy'
   resources :reviews
-  resources :shoes
+  resources :shoes do
+    resources :reviews , only:  [:new, :index]
+  end
   resources :brands
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
