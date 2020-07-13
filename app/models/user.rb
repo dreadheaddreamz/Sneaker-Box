7010820c6f482f5cb6cceffa36593379299a0176
dreadheaddreamz <github email address>
2020-07-12 21:36:42 -0500

    added email to user table and google auth feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
     has_many :kicks
     validates :username, uniqueness: true,  presence: true
     has_secure_password
+
+
+    def self.create_by_google_omniauth(auth)
+        self.find_or_create_by(username: auth[:info][:email]) do |u|
+          u.password = SecureRandom.hex
+        end
+      end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
 
+  get '/auth/google_oauth2/callback' => 'sessions#omniauth' 
+
 
   resources :reviews
   resources :shoes do
